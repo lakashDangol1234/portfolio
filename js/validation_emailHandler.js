d803739c83c00c924b7ce5e18485152c3f594b26
lakash dangol <lakash.dangol@gmail.com>
2024-11-26 17:36:22 +0545

    Fix validation problem after submitting contact form: reset form , making button disable and removing green mark border after successful submission of form

--- js/validation_emailHandler.js
+++ js/validation_emailHandler.js
@@ -68,3 +68,66 @@ nameInput.addEventListener("input", validateName);
 emailInput.addEventListener("input", validateEmail);
 subjectInput.addEventListener("input", validateSubject);
 messageInput.addEventListener("input", validateMessage);
+
+
+
+// Email handler
+// Emailjs
+(function(){
+    emailjs.init({
+      publicKey: "iU23aOkfO8hlU82B9",
+    });
+ })();
+
+function sendMail(e){
+    e.preventDefault();
+    let params={
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        subject: document.getElementById('subject').value,
+        message: document.getElementById('message').value
+    }
+
+const serviceID = "service_o7y3jfu";
+const templateID = "template_cqv3xae";
+
+emailjs.send(serviceID,templateID,params)
+.then(res=>{
+    document.getElementById('contact_form').reset();
+    document.getElementById('submit_contact_btn').disabled=true;
+    document.querySelectorAll('#contact_form input , #contact_form textarea').forEach((element)=>{
+        element.classList.remove('is-valid');
+    })
+    isEmailValid = false;
+    isEmailValid = false;
+    isMessageValid = false;
+    isSubjectValid = false;
+    toastTrigger("success");
+})
+.catch(error=>{
+    toastTrigger("error");
+})
+
+}
+
+// Popping Toast After sending email
+const contactToast = document.getElementById('contactToast')
+const toastBootstrap = bootstrap.Toast.getOrCreateInstance(contactToast);
+
+function toastTrigger(status){
+    let toastBody = document.querySelector('.toast-body');
+    if (status==="success") {
+        toastBody.innerText="Thank You ! For sending Email . I will reply you if required.";
+        toastBody.classList.add('text-success');
+    }
+    else if(status === "error"){
+         toastBody.innerText="Sorry ! Your Email wasn't sent. Please Try Again Later";
+        toastBody.classList.add('text-danger');
+    }
+    toastBootstrap.show();
+    
+}
+
+
+// Adding Event listener
+document.getElementById('contact_form').addEventListener('submit',sendMail);
